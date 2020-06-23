e0c95cc8584481d5264f27cc87a5d50bcae5129d
charlenne alexander <charlennep@gmail.com>
2020-06-23 09:59:17 -0500

    added more users to the ap

--- src/util/api.js
+++ src/util/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   search: function () {
-    return axios.get("https://randomuser.me/api/?results=4");
+    return axios.get("https://randomuser.me/api/?results=20");
   },
 };
